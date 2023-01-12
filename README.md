5dba4765e1eec69648be845dd8d5966b4fface80
Jonathan Potter <jonathan.d.potter@outlook.com>
2023-01-12 10:19:03 -0500

    Updated README.md and about page

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blog-Front
+
+This website is a single page application built with React.js and TypeScript. It is my second version of the Odin Project blog project. The first was fully functional, but I wanted to update the code with new things that I have learned since originally building it. I also used this as an opportunity to further my practice with Bootstrap.
+
+## Technologies used
+
+***
+
+This app uses React Router for page routing, Redux for state management, Axios for api calls, react-markdown to display markdown, and Bootstrap for styling. The back-end is built with Express.js and manages a MongoDB noSQL database to make a full-stack MERN application.
+
+## Functionality
+
+***
+
+When a user opens the home page they can view all published blog posts. The newest posts are at the top, and the user can scroll down to see older posts. If the user clicks on the title of a post, the post will open in full on a seperate page. If the user is logged in and is the author of the post, the user will be able to edit or delete the post. The user can see all posts by a particular author by clicking on the author's name. A user can also make a new post and if they set it as published, it will show up on the home page.
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,25 +1,30 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+
+const aboutMarkdown = `# Blog-Front
+
+***
+
+This website is a single page application built with React.js and TypeScript. It is my second version of the Odin Project blog project. The first was fully functional, but I wanted to update the code with new things that I have learned since originally building it. I also used this as an opportunity to further my practice with Bootstrap.
+
+## Technologies used
+
+***
+
+This app uses React Router for page routing, Redux for state management, Axios for api calls, react-markdown to display markdown, and Bootstrap for styling. The back-end is built with Express.js and manages a MongoDB noSQL database to make a full-stack MERN application.
+
+## Functionality
+
+***
+
+When a user opens the home page they can view all published blog posts. The newest posts are at the top, and the user can scroll down to see older posts. If the user clicks on the title of a post, the post will open in full on a seperate page. If the user is logged in and is the author of the post, the user will be able to edit or delete the post. The user can see all posts by a particular author by clicking on the author's name. A user can also make a new post and if they set it as published, it will show up on the home page.`;
 
 const About = () => {
   return (
-    <div>
-      <div className="container">
-        <h2>About</h2>
-        <p>
-          This website is a single page application built with React.js and
-          TypeScript. It is my second version of the Odin Project blog project. The first was fully functional, but I wanted to update the code with new things that I have learned since originally building it. I also used this as an opportunity to further my practice with Bootstrap.
-        </p>
-        <hr />
-        <h3>Technologies used</h3>
-        <p>
-          This app uses React Router for page routing, Redux for state management,
-          Axios for api calls, react-markdown to display markdwon, and Bootstrap for styling. The back-end is built with Express.js and manages a MongoDB noSQL database to make a full-stack MERN application.
-        </p>
-        <hr />
-        <h3>Functionality</h3>
-        <p>When a user opens the home page they can view all published blog posts. The newest posts are at the top, and the user can scroll down to see older posts. If the user clicks on the title of a post, the post will open in full on a seperate page. If the user is logged in and is the author of the post, the user will be able to edit or delete the post. The user can see all posts by a particular author by clicking on the author's name. A user can also make a new post and it they set it as published, it will show up on the home page.</p>
-      </div>
-    </div>
+    <ReactMarkdown
+      children={aboutMarkdown}
+      components={{ h1: "h2", h2: "h3" }}
+    />
   );
 };
 
